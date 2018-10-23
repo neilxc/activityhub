bfb2a0be96f5dd92bf1492090f99772941ed9720
trycatchlearn <trycatchlearn@outlook.com>
2018-10-23 20:15:40 +0100

    Initial Commit

---
+++ Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDataAccessServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(connectionString);
+            });
+        }
+    }
+}
